df4eac013144d2e790636f39b6f7cbf5d1f4c06e
schmitts <schmitts@rptu.de>
2024-05-26 15:47:01 +0200

    Started working on training

--- src/model.jl
+++ src/model.jl
@@ -11,13 +11,73 @@ function build_model(no_teams::Int, input_dim::Int)
     Lux.Chain(
         # Embedding layers for home and away teams
         Lux.Parallel(
-            Lux.Embedding(no_teams, nodes_emb),                 # Embedding for home team
-            Lux.Embedding(no_teams, nodes_emb)                  # Embedding for away team
+            Lux.Embedding(no_teams => nodes_emb),                 # Embedding for home team
+            Lux.Embedding(no_teams => nodes_emb)                  # Embedding for away team
         ),
-        Lux.Flatten(),  # Flatten the embeddings
+        Lux.FlattenLayer(),  # Flatten the embeddings
         Lux.Dense(input_dim - 2 + (2*nodes_emb), 128, relu),    
         Lux.Dense(128, 64, relu),
         Lux.Dense(64, 32, relu),
-        Lux.Dense(32, 2)                                        # Two outputs: home score and away score -> here relu too?
+        Lux.Dense(32, 2)                                        # Two outputs: home score and away score -> here relu too to ensure positive value? Or 10x2 output for numbers 0-9)
     )
+end
+
+# Loss function
+function compute_loss(model, ps, st, data)
+    # Apply model to data
+    y_pred, st = apply(model, data[1], ps, st)
+
+    # Define loss functions 
+    lossf_scrs = L2DistLoss()
+    lossf_res = ModifiedHuberLoss()
+    
+    # Calculate loss 
+    diff_data = data[2][:,1].-data[2][:,2]
+    diff_pred = y_pred[:,1].-y_pred[:,2]
+    diff = sign.(diff_data.*diff_pred) .* abs.(diff_data.-diff_pred)
+    loss_score = sum([lossf_scrs.(y_pred[:,i], data[2][:,i]) for i in 1:2])
+    loss_result = lossf_res.(diff)
+    accuracy = sum(diff .≥ 0) / length(diff)
+    return loss_score .+ loss_result, st, (;accuracy=accuracy)
+end
+
+# Function to train model
+function train_model(model, data; epochs=100, batch_size=64)
+    # Create training, validation and test sets 
+    #=  (80% training, 10% validation, 10% test)
+        Currently: first 80 % of games, remaining 20 % randomly split
+        Future: better methods for times series data?=#
+    sort!(data, :date)
+    what_train = [1:floor(Int,0.8*size(data,1));]
+    what_val, what_test = splitobs(shuffleobs((floor(Int,0.8*size(data,1))+1):size(data,1)),at=0.5)
+    
+    # Create data loaders
+    features = Matrix(data[:,[:id_home_team,:id_away_team,:date,:is_friendly]])
+    labels = Matrix(data[:,[:home_score,:away_score]])
+    train_loader = DataLoader((features[what_train,:], labels[what_train,:]), batchsize=batch_size, shuffle=true)
+    val_loader = DataLoader((features[what_val,:], labels[what_val,:]), batchsize=batch_size)
+    test_loader = DataLoader((features[what_test,:], labels[what_test,:]), batchsize=batch_size)
+
+    # Create train state
+    tstate = Lux.Experimental.TrainState(Xoshiro(0), model, Adam(0.01f0))
+
+    for epoch in 1:epochs
+        # Train the model
+        for (x, y) in train_loader
+            gs, loss, _, train_state = Lux.Experimental.compute_gradients(
+                AutoZygote(), compute_loss, (x, y), tstate)
+            train_state = Lux.Experimental.apply_gradients!(tstate, gs)
+
+            @printf "Epoch [%3d]: Loss %4.5f\n" epoch loss
+        end
+
+        # Validate the model
+        st_ = Lux.testmode(train_state.states)
+        for (x, y) in val_loader
+            loss, st_, ret = compute_loss(model, train_state.parameters, st_, (x, y))
+            acc = accuracy(ret.y_pred, y)
+            @printf "Validation: Loss %4.5f Accuracy %4.5f\n" loss acc
+        end
+    end
+    error()
 end
